4e5727e688a4c2ac4229487679d9855287f40952
Andreas Schroeer <AndreasSchroeer@users.noreply.github.com>
2024-01-10 16:24:30 +0100

    Code refactoring to more object oriented code, Added error handling

--- tlstest.sh
+++ tlstest.sh
@@ -25,42 +25,66 @@
 
 set -eu
 
-# Ask user for filename with hosts and ports to scan
-echo "Please enter the filename with hosts to scan:"
-read -r hosts
+main() {
+	echo "Please enter the filename with hosts to scan:"
+	read -r hosts
 
-# Date and time setting to append to output filename
-dati=$(date '+%Y%m%d_%H%M%S')
+	# Date and time setting to append to output filename
+	dati=$(date '+%Y%m%d_%H%M%S')
 
-# Declare output filename
-outputfile=${hosts%%.*}"_tlstest_$dati.csv"
+	# Declare output filename
+	outputfile="${hosts%%.*}_tlstest_$dati.csv"
+	errorfile="${hosts%%.*}_tlstest_$dati.err"
 
-# Perform a scan to each host and port from delivered file and create output csv file
-# sed removes carriage return if inside file before performing the scans
-sed -r -e 's/\r//g' "$hosts" |
-while read -r -d';' host && read -r port; do
-	/opt/testssl.sh/testssl.sh --quiet -E -S --mapping no-openssl --csvfile "$outputfile" --append "$host":"$port"
-done
+	# Perform a scan to each host and port from delivered file and create output csv file
+	# sed removes carriage return if inside file before performing the scans
+	sed -r -e 's/\r//g' "$hosts" |
+	while read -r -d';' host && read -r port; do
+		testssl_scan "$host" "$port" "$outputfile" "$errorfile"
+	done
 
-# Remove unnecessary information
-# First expression > Change / into a separator to separate FQDN and IP-Address
-# Second expression > Change three spaces into a separator
-# Third expression > Print only lines beginning with "cipher to outputfile
-# Fourth expression > Print only lines beginning with "cert_signatureAlgorithm to outputfile
-# Fifth expression > Print only lines beginning with "cert_keySize to outputfile
-sed -i -r -n -e 's/[/]/","/' -e 's/  +/","/g' -e '/"cipher.*/p' -e '/"cert_signatureAlgorithm.*/p' -e '/"cert_keySize.*/p' "$outputfile"
-# First expression > Remove ,"","" inside the file to remove empty fields
-# Second experession > Remove unecessary information
-sed -i -e 's/,"",""//g' -e 's/ (exponent is 65537)//g' "$outputfile"
+	# Remove unnecessary information
+	# First expression > Change / into a separator to separate FQDN and IP-Address
+	# Second expression > Change three spaces into a separator
+	# Third expression > Print only lines beginning with "cipher to outputfile
+	# Fourth expression > Print only lines beginning with "cert_signatureAlgorithm to outputfile
+	# Fifth expression > Print only lines beginning with "cert_keySize to outputfile
+	sed -i -r -n -e 's/[/]/","/' -e 's/  +/","/g' -e '/"cipher.*/p' -e '/"cert_signatureAlgorithm.*/p' -e '/"cert_keySize.*/p' "$outputfile"
+	# First expression > Remove ,"","" inside the file to remove empty fields
+	# Second experession > Remove unecessary information
+	sed -i -e 's/,"",""//g' -e 's/ (exponent is 65537)//g' "$outputfile"
 
-# Set temporary file
-tempfile=$(mktemp)
+	# Set temporary file
+	tempfile=$(mktemp)
 
-# Remove unnecessary columns > Copy only needed columns inside temporary file
-cut -d',' -f1-4,6-11 "$outputfile" > "$tempfile"
+	# Remove unnecessary columns > Copy only needed columns inside temporary file
+	cut -d',' -f1-4,6-11 "$outputfile" > "$tempfile"
 
-# Add titles to the first line
-firstline="testssl NAME","FQDN","IP-Address","Port","Value","IANA-Notation","Cipher-Suite","Key Exchange","Encryption Algorythm","Encryption Key Size"
+	# Add titles to the first line
+	firstline="testssl NAME","FQDN","IP-Address","Port","Value","IANA-Notation","Cipher-Suite","Key Exchange","Encryption Algorythm","Encryption Key Size"
 
-# Write tempfile to outputfile
-echo "$firstline" | cat - "$tempfile" > "$outputfile"
+	# Write tempfile to outputfile
+	echo "$firstline" | cat - "$tempfile" > "$outputfile"
+
+	print_errorlog "$errorfile"
+}
+
+# $1: Hostname or IP-address
+# $2: Portnumber
+# $3: Name of Outputfile
+# $4: Name of errorfile
+testssl_scan() {
+	/opt/testssl.sh/testssl.sh --quiet -E -S --mapping no-openssl --csvfile "$3" --append "$1":"$2" 2>>"$4" || {
+		echo "Error occured, please view error log $4"
+	}
+}
+
+# $1: Name of errorfile
+print_errorlog() {
+	echo "Content from errorlog $1"
+	echo "----------------------------------"
+	echo
+	cat "$1"
+}
+
+main "$@"
